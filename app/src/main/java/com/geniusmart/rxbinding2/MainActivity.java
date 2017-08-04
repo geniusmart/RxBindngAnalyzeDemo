a1824ab488535c962090c2093eaa6b6206a78b4d
geniusmart <270391484@qq.com>
2017-08-04 17:53:20 +0800

    commit

--- app/src/main/java/com/geniusmart/rxbinding2/MainActivity.java
+++ app/src/main/java/com/geniusmart/rxbinding2/MainActivity.java
@@ -1,21 +1,36 @@
 package com.geniusmart.rxbinding2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.jakewharton.rxbinding2.view.RxView;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    public static final String TAG = "RxBindngAnalyzeDemo";
+
     @BindView(R.id.button)
     Button button;
+    @BindView(R.id.editText)
+    EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +40,33 @@ public class MainActivity extends AppCompatActivity {
         RxView.clicks(button).subscribe(new Consumer<Object>() {
             @Override
             public void accept(Object o) throws Exception {
-                Toast.makeText(MainActivity.this,"button",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "button", Toast.LENGTH_SHORT).show();
             }
         });
+
+        RxTextView.textChanges(editText)
+                .map(new Function<CharSequence, String>() {
+                    @Override
+                    public String apply(CharSequence charSequence) throws Exception {
+                        return charSequence.toString().toUpperCase();
+                    }
+                })
+                .debounce(1000, TimeUnit.MILLISECONDS)
+                .flatMap(new Function<String, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(String s) throws Exception {
+                            //Thread.sleep(2000);
+                        return Observable.just("after:10s"+s);
+                    }
+                })
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String result) throws Exception {
+                        Log.i(TAG, result);
+                    }
+                });
 
     }
 }
